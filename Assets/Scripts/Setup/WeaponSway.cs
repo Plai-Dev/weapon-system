8c63ffae14ce247f4bf034b2506773a95fb1639b
Zarashigal J <120185445+Zaraaashiigal@users.noreply.github.com>
2023-01-06 08:26:41 +0100

    Fix for new InputSystem!
    The original code of your example was quite well made, but unity updated and now it complains about the input system.
    I took the liberty to quickly fix it up for you, so you and others do not have to!

--- Assets/Scripts/Setup/WeaponSway.cs
+++ Assets/Scripts/Setup/WeaponSway.cs
@@ -1,28 +1,41 @@
+/* Importing Namespaces. */
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class WeaponSway : MonoBehaviour {
+/* Setup our class */
+public class WeaponSway : MonoBehaviour
+{
 
+    /* Sway settings for use in Inspector. */
     [Header("Sway Settings")]
     [SerializeField] private float speed;
     [SerializeField] private float sensitivityMultiplier;
 
+    /* Variables! Oh deary me! */
     private Quaternion refRotation;
-
     private float xRotation;
     private float yRotation;
 
-    private void Update()
-    {
-        // get mouse input
-        float mouseX = Input.GetAxisRaw("Mouse X") * sensitivityMultiplier;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * sensitivityMultiplier;
+    /* Update function. Gets ran every tick! */
+    private void Update() {
 
+        /* The following line makes the jump from the old Input Package to the new UnityEngine.InputSystem. */
+        Vector2 mousePosition = Mouse.current.delta.ReadValue();
+
+        /* Get Mouse-Delta, Multiply such with the Sensitivity Multiplier we set in the inspector. */
+        float mouseX = mousePosition.x * sensitivityMultiplier;
+        float mouseY = mousePosition.y * sensitivityMultiplier;
+
+        /* Manipulate the Quaternions. */
 		Quaternion rotationX = Quaternion.AngleAxis(-mouseY, Vector3.right);
 		Quaternion rotationY = Quaternion.AngleAxis(mouseX, Vector3.up);
 
+        /* Put them into a package! */
 		Quaternion targetRotation = rotationX * rotationY;
 
+        /* We SWAYYYYS! :O */
         transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, speed * Time.deltaTime);
     }
+
 }
